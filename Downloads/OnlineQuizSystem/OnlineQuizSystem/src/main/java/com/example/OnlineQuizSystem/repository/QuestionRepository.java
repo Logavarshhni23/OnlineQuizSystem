4855ead633f0c30fd871a3d48e42b71d9bfe72fc
Logavarshhni23 <logavarshhnis2007@gmail.com>
2026-06-09 14:40:43 +0530

    commit

--- Downloads/OnlineQuizSystem/OnlineQuizSystem/src/main/java/com/example/OnlineQuizSystem/repository/QuestionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.OnlineQuizSystem.repository;
-
-import com.example.OnlineQuizSystem.model.Question;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface QuestionRepository extends JpaRepository<Question, Long> {
-}
