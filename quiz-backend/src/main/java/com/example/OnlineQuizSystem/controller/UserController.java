597a6ba931152225ce7a92370ed18df62589bf3c
Logavarshhni23 <logavarshhnis2007@gmail.com>
2026-06-11 15:23:54 +0530

    connected backend to frontend

--- quiz-backend/src/main/java/com/example/OnlineQuizSystem/controller/UserController.java
+++ quiz-backend/src/main/java/com/example/OnlineQuizSystem/controller/UserController.java
@@ -7,6 +7,7 @@ import com.example.OnlineQuizSystem.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/api/auth")
 public class UserController {
