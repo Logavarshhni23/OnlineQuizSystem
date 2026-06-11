b2fb3f900422641630d308f5dedd3ac54aa72b01
Logavarshhni23 <logavarshhnis2007@gmail.com>
2026-06-11 18:24:43 +0530

    commit

--- quiz-backend/src/main/java/com/example/OnlineQuizSystem/config/SecurityConfig.java
+++ quiz-backend/src/main/java/com/example/OnlineQuizSystem/config/SecurityConfig.java
@@ -2,7 +2,6 @@ package com.example.OnlineQuizSystem.config;
 
 import com.example.OnlineQuizSystem.security.JwtAuthFilter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
